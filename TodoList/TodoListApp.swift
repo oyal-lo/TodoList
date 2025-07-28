7e6166d6c8e2648566f8dea7d84cfad444e26b00
Olive Luong <olivee.yan@gmail.com>
2025-07-28 09:56:18 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Olive Luong on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
